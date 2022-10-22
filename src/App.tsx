55b98b2e5cc6c8f5feae0eafb65ba3760dfd394d
shkholikov <imshakhzod@gmail.com>
2022-10-22 20:18:17 +0200

    1st main commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { Grid, Header, Table } from "semantic-ui-react";
+import gitdata from "./gitdata.json";
+import { CopyBlock, dracula } from "react-code-blocks";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [data, setData] = useState<any>();
+
+	useEffect(() => {
+		setData(gitdata);
+	}, []);
+
+	const createApplicationTable = (type: string): React.ReactElement => (
+		<Table inverted celled={false} selectable>
+			<Table.Header>
+				<Table.Row>
+					<Table.HeaderCell>{type}</Table.HeaderCell>
+					<Table.HeaderCell></Table.HeaderCell>
+				</Table.Row>
+			</Table.Header>
+
+			<Table.Body>
+				{data?.[`${type}`].map((item: any, idx: string | number) => (
+					<Table.Row id={idx}>
+						<Table.Cell width={6}>
+							<CopyBlock text={item.code} language={"bash"} codeBlock theme={dracula} showLineNumbers={false} />
+						</Table.Cell>
+						<Table.Cell width={8}>{item.description}</Table.Cell>
+					</Table.Row>
+				))}
+			</Table.Body>
+		</Table>
+	);
+
+	return (
+		<Grid container>
+			<Grid.Row centered>
+				<Grid.Column>
+					<br />
+					<Header size="huge" as="h1">
+						💻 GIT-CHEATSHEET
+					</Header>
+					<Header as="h3">🧑🏻‍💻 Stop looking for a git commands and just use this Git-Cheatsheet</Header>
+				</Grid.Column>
+			</Grid.Row>
+			<Grid.Row>
+				<Grid.Column>
+					{data ? createApplicationTable("GIT BASICS") : ""}
+					{data ? createApplicationTable("UNDOING CHANGES") : ""}
+					{data ? createApplicationTable("REWRITING GIT HISTORY") : ""}
+					{data ? createApplicationTable("GIT BRANCHES") : ""}
+					{data ? createApplicationTable("REMOTE REPOSITORIES") : ""}
+				</Grid.Column>
+			</Grid.Row>
+		</Grid>
+	);
 }
 
 export default App;
