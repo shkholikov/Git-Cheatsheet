3562cf131cd187a10b8589a5ad3da7a35f6c735b
shkholikov <imshakhzod@gmail.com>
2022-10-29 17:56:27 +0200

    added particles background

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import { Divider, Grid, Header, Table } from "semantic-ui-react";
 import gitdata from "./gitdata.json";
 import { CopyBlock, dracula } from "react-code-blocks";
+import Particles from "react-particles";
+import type { Container, Engine } from "tsparticles-engine";
+import { loadFull } from "tsparticles";
 
 function App() {
 	const [data, setData] = useState<any>();
 
 	useEffect(() => {
 		setData(gitdata);
+	}, []);
+
+	const particlesInit = useCallback(async (engine: Engine) => {
+		await loadFull(engine);
+	}, []);
+
+	const particlesLoaded = useCallback(async (container: Container | undefined) => {
+		await console.log(container);
 	}, []);
 
 	const createApplicationTable = (type: string): React.ReactElement => (
@@ -34,6 +45,81 @@ function App() {
 
 	return (
 		<>
+			<Particles
+				id="tsparticles"
+				init={particlesInit}
+				loaded={particlesLoaded}
+				options={{
+					background: {
+						opacity: 0
+					},
+					fpsLimit: 120,
+					interactivity: {
+						events: {
+							onClick: {
+								enable: true,
+								mode: "push"
+							},
+							onHover: {
+								enable: true,
+								mode: "repulse"
+							},
+							resize: true
+						},
+						modes: {
+							push: {
+								quantity: 4
+							},
+							repulse: {
+								distance: 200,
+								duration: 0.4
+							}
+						}
+					},
+					particles: {
+						color: {
+							value: "#000000"
+						},
+						links: {
+							color: "#000000",
+							distance: 150,
+							enable: true,
+							opacity: 0.5,
+							width: 1
+						},
+						collisions: {
+							enable: true
+						},
+						move: {
+							direction: "none",
+							enable: true,
+							outModes: {
+								default: "bounce"
+							},
+							random: false,
+							speed: 2,
+							straight: false
+						},
+						number: {
+							density: {
+								enable: true,
+								area: 800
+							},
+							value: 80
+						},
+						opacity: {
+							value: 0.5
+						},
+						shape: {
+							type: "circle"
+						},
+						size: {
+							value: { min: 1, max: 5 }
+						}
+					},
+					detectRetina: true
+				}}
+			/>
 			<Grid container>
 				<Grid.Row centered>
 					<Grid.Column>
